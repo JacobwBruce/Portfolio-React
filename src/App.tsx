3e42b1184d54dac9c513fabd62e0096210a7fff2
JacobwBruce <jacobwbruce@hotmail.com>
2021-03-07 11:45:52 -0500

    Theme icon animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import About from './components/About';
 import Masthead from './components/Masthead';
-import './css/App.css';
-import { WiDaySunny, WiMoonWaningCrescent3 } from 'react-icons/wi';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Projects from './components/Projects';
+import { AnimatePresence, motion } from 'framer-motion';
+import { WiDaySunny, WiMoonWaningCrescent3 } from 'react-icons/wi';
+import './css/App.css';
 
 function App() {
     if (JSON.parse(window.localStorage.getItem('theme')!) === 'dark') {
@@ -23,6 +24,18 @@ function App() {
         window.localStorage.setItem('theme', JSON.stringify(!isDarkTheme));
     };
 
+    const themeVariants = {
+        initial: {
+            x: '-100vw',
+        },
+        animate: {
+            x: 0,
+        },
+        exit: {
+            x: '100vw',
+        },
+    };
+
     return (
         <div className={`App ${isDarkTheme ? 'dark-theme' : 'light-theme'}`}>
             <Masthead />
@@ -32,21 +45,39 @@ function App() {
             <Footer />
 
             <div className='fixed-top'>
-                {!isDarkTheme ? (
-                    <WiDaySunny
-                        size={50}
-                        className='m-3'
-                        style={{ cursor: 'pointer' }}
-                        onClick={changeTheme}
-                    />
-                ) : (
-                    <WiMoonWaningCrescent3
-                        size={50}
-                        className='m-3'
-                        style={{ cursor: 'pointer' }}
-                        onClick={changeTheme}
-                    />
-                )}
+                <AnimatePresence exitBeforeEnter>
+                    {!isDarkTheme ? (
+                        <motion.div
+                            key='sun'
+                            variants={themeVariants}
+                            initial='initial'
+                            animate='animate'
+                            exit='exit'
+                        >
+                            <WiDaySunny
+                                size={50}
+                                className='m-3'
+                                style={{ cursor: 'pointer' }}
+                                onClick={changeTheme}
+                            />
+                        </motion.div>
+                    ) : (
+                        <motion.div
+                            key='moon'
+                            variants={themeVariants}
+                            initial='initial'
+                            animate='animate'
+                            exit='exit'
+                        >
+                            <WiMoonWaningCrescent3
+                                size={50}
+                                className='m-3'
+                                style={{ cursor: 'pointer' }}
+                                onClick={changeTheme}
+                            />
+                        </motion.div>
+                    )}
+                </AnimatePresence>
             </div>
         </div>
     );
